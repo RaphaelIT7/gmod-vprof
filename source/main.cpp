cd6eaf3fffac218f27e1f19d1657bc5e917f6b45
RaphaelIT7 <raphael.i.t@outlook.de>
2024-05-04 05:40:47 +0200

    Testing a fix for the ConVars missing

--- source/main.cpp
+++ source/main.cpp
@@ -1,15 +1,29 @@
+#include <GarrysMod/InterfacePointers.hpp>
 #include "main.h"
 
 void VProfLoad()
 {
+	cvar = InterfacePointers::Cvar();
+
 	if (vprof_showhooks.GetBool())
 		AddLuaHooks();
 
 	AddVProfExport();
+
+	VProfConVar(vprof_exportreport);
+	VProfConVar(vprof_showhooks);
 }
 
 void VProfUnload()
 {
 	RemoveLuaHooks();
 	RemoveVProfExport();
+}
+
+void VProfConVar(ConVar convar)
+{
+	if (!convar.IsRegistered())
+	{
+		cvar->RegisterConCommand((ConCommandBase*)&convar);
+	}
 }
--- source/main.h
+++ source/main.h
@@ -4,10 +4,13 @@
 
 extern void VProfLoad();
 extern void VProfUnload();
+extern void VProfConVar(ConVar convar);
 
+extern ICvar* cvar;
 extern ConVar vprof_showhooks;
 extern void AddLuaHooks();
 extern void RemoveLuaHooks();
 
+extern ConVar vprof_exportreport;
 extern void AddVProfExport();
 extern void RemoveVProfExport();
